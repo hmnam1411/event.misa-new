3eb38ce07015dd57bd81f8e5d9ec1d52a4d6e9be
NAM HOANG MINH <hmnam@misa-group.vn>
2023-02-16 09:37:20 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <BannerHome />
+    <FilterHome />
+    <UpcommingEvent />
+    <FinishEvent />
+  </div>
+</template>
+
+<script>
+import '../assets/style/index.css'
+
+import FilterHome from '~/pages/homepage/FilterHome.vue';
+import FinishEvent from '~/pages/homepage/FinishEvent.vue';
+import UpcommingEvent from '~/pages/homepage/UpcommingEvent.vue';
+import BannerHome from '~/pages/homepage/BannerHome.vue';
+
+export default {
+  name: 'IndexPage',
+  components: {
+    FilterHome,
+    UpcommingEvent,
+    FinishEvent,
+    BannerHome,
+  }
+}
+</script>
